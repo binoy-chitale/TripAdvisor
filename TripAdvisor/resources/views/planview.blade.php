1976da6e6671768d15c410914b3dcc84b7a0f870
Binoy Chitale <binoy.c@media.net>
2017-08-20 02:31:09 -0400

    added draggable itenerary

--- TripAdvisor/resources/views/planview.blade.php
+++ TripAdvisor/resources/views/planview.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row" style="display:flex;">
 		@foreach($itenerary as $dayplan)
-			<div class="panel-heading" style="text-align:center;"><h1>{{$dayplan['day']->format("d-m-Y")}}</h1> </div>
-			<div class="panel-body">
+			<div class="panel panel-default">	
+				<p class="ui-state-default">{{$dayplan['day']->format('d-m-Y')}}</p>
+				<ul class="sortable">
 				@foreach($dayplan['plan'] as $item)
-					<p>{{$item->name}}&nbsp {{$item->startofvisit}}&nbsp {{$item->endofvisit}}</p>
+					<li class="ui-state-default">{{$item->name}}&nbsp {{$item->startofvisit}}&nbsp {{$item->endofvisit}}</li>
 				@endforeach
+				</ul>
 			</div>
-		@endforeach    
+		@endforeach
 	</div>
 
 </div>
+@endsection
+@section('scripts')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="{{ asset('js/drag.js') }}"></script>
+@endsection
+@section('styles')
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<style type="text/css">
+  .sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
+  .sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; }
+  .sortable li span { position: absolute; margin-left: -1.3em; }
+</style>
 @endsection
