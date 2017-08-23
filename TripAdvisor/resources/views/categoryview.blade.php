871a94358a6a85f9993be9d7a12844593faa92a4
Bansi Shah <bansishah0695@gmail.com>
2017-08-23 07:14:04 -0400

    Category picker updated UI and search bar

--- TripAdvisor/resources/views/categoryview.blade.php
+++ TripAdvisor/resources/views/categoryview.blade.php
@@ -4,18 +4,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading" style="text-align:center;">{{$name}}</div>
+                <div class="panel-heading catpickheading" style="text-align:center; background: #1b1e24; color:white; font-weight: 600; border-radius:4px; border:2px solid; border-color:#1b1e24; text-transform:uppercase;">{{$name}}</div>
                 	{!!Form::open(array('action' => 'CategoryController@getCategories', 'url'=>'/plan/'.$name))!!}
                 	<div class="panel-body catpickpanel">
-                        <div class="layer"></div>
                         <div style="display:flex;">
-                            <input type="date" name="start_date" class="datepick" value="{{csrf_token()}}"><input type="date" name="end_date" class="datepick" value="{{csrf_token()}}"><br>
+                            <input type="date" name="start_date" id="datepicker" class="datepick" value="{{csrf_token()}}"><input type="date" name="end_date" id= "datepicker" class="datepick" value="{{csrf_token()}}"><br>
                         </div><br>
                         @foreach($categories as $category)
 						            <label for="btn_{{$category->name}}" class="catpicker">{{$category->name}}</label> 
                                     <input class="cbox" id="btn_{{$category->name}}"type="checkbox" value="{{csrf_token()}}" name= "catvalues[]"/>
                     	@endforeach
-						 <br><button type="submit" value="submit" class="btn btn-plan" style="font-size:20px;">Plan!</button>         
+						 <br><button type="submit" value="submit" class="btn-plan btn" id="plan" style="font-size:14px; margin-top: 20px; border: 2px solid; border-radius: 4px; border-color:#454545; font-weight:600;">Plan!</button>         
                     </div>
                     {!!Form::hidden('attractions',$attractions)!!}
                     {!!Form::close()!!}
